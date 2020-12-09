c08a56a04d90d97d269e611ce0f18ef61502b43b
millu <millun.atluri@gmail.com>
2020-12-08 21:39:49 -0700

    day1 pt2

--- day1/day1.py
+++ day1/day1.py
@@ -7,11 +7,12 @@ def read_file(path):
 			nums[int(line)] = 0
 	return nums
 
-
 def find_answer(nums):
 	for n in nums:
-		if 2020-n > nums:
-			return (n*(2020-n))
+		new_sum = 2020-n
+		for y in nums:
+			if new_sum-y in nums:
+				return (n*y*(2020-n-y))
 
 nums = read_file(file_path)
 print(find_answer(nums))
