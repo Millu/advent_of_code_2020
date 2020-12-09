e310d0a6da20b9b3d55c776c3cb1940091ba56e5
millu <millun.atluri@gmail.com>
2020-12-08 22:34:52 -0700

    day2 pt2

--- day2/day2.py
+++ day2/day2.py
@@ -11,13 +11,13 @@ def verify(passwords):
 	valid_passwords = 0
 	for p in passwords:
 		rule = p.split(':')[0]
-		password_text = p.split(':')[1]
+		password_text = p.split(':')[1].strip()
 		rule_breakdown = rule.split()
-		min_repeats = int(rule_breakdown[0].split('-')[0])
-		max_repeats = int(rule_breakdown[0].split('-')[1])
+		min_pos = int(rule_breakdown[0].split('-')[0]) - 1
+		max_pos = int(rule_breakdown[0].split('-')[1]) - 1
 		letter = rule_breakdown[1][0]
-		count = p.count(letter) - 1
-		if count >= min_repeats and count <= max_repeats:
+		# print("'"+password_text+"'", letter, min_pos, max_pos)
+		if bool(password_text[min_pos] == letter) ^ bool(password_text[max_pos] == letter):
 			valid_passwords = valid_passwords + 1
 	return valid_passwords
 
