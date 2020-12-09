f70c2d186085796ca970b2621d6b75f57bbcbc5e
millu <millun.atluri@gmail.com>
2020-12-08 21:26:46 -0700

    day1 pt1

--- day1/day1.py
+++ day1/day1.py
@@ -1,9 +1,17 @@
-path = 'input.txt'
+file_path = 'input.txt'
 
-num = []
+def read_file(path):
+	nums = {}
+	with open(path) as f:
+		for line in f:
+			nums[int(line)] = 0
+	return nums
 
-with open(path) as f:
-	for line in f:
-		num.append(int(line))
 
-print(num)
+def find_answer(nums):
+	for n in nums:
+		if 2020-n > nums:
+			return (n*(2020-n))
+
+nums = read_file(file_path)
+print(find_answer(nums))
